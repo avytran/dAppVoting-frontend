9659e57fa341effdc0240d225d84511d105db8dd
Anh Vy Tran <tr.anhvy.work@gmail.com>
2026-05-05 22:06:59 +0700

    Feature/ Election Metrics (#1)
    * feat: add global css
    
    * feat: add mock data
    
    * feat: implement election metrics component
    
    * chore: remove example components

---
+++ front-end/src/mocks/candidates.js
@@ -0,0 +1,20 @@
+export const MOCK_CANDIDATES = [
+  {
+    id: 1,
+    name: "Marcus Thorne",
+    votes: 450,
+    image: "https://api.dicebear.com/7.x/avataaars/svg?seed=Marcus"
+  },
+  {
+    id: 2,
+    name: "Elena Vance",
+    votes: 350,
+    image: "https://api.dicebear.com/7.x/avataaars/svg?seed=Elena"
+  },
+  {
+    id: 3,
+    name: "Julian Lee",
+    votes: 200,
+    image: "https://api.dicebear.com/7.x/avataaars/svg?seed=Julian"
+  }
+];

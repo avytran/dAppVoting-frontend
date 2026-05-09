440c04cb1447ecd84a08f64b94ad56b8d2db4a2d
avytran <tr.anhvy.work@gmail.com>
2026-05-09 23:51:30 +0700

    feat: implement pages routing

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'; // Bỏ BrowserRouter/Router ở đây
 import { Dashboard } from './pages/Dashboard';
+import { AdminPortal } from './pages/AdminPortal';
 import './App.css';
 
 function App() {
   return (
     <div className="app-container">
-      <Dashboard />
+      <Routes>
+        {/* Main Page */}
+        <Route path="/" element={<Dashboard />} />
+        
+        {/* Admin Management Portal */}
+        <Route path="/admin" element={<AdminPortal />} />
+      </Routes>
     </div>
   );
 }

aee4d0e6e50e0d965c7e7b0e1be261b7047e1ecd
avytran <tr.anhvy.work@gmail.com>
2026-05-09 21:38:16 +0700

    refactor: change components location

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import AdminTitleCard from './components/AdminCards/AdminTitleCard';
-import FinalizationCard from './components/AdminCards/FinalizationCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> 
-      
-      {/* Phần nội dung chính nằm ở giữa */}
-      <main style={{ padding: '20px 40px', minHeight: '70vh' }}>
-        <AdminTitleCard />
-        
-      
-        <FinalizationCard />
-      </main>
-
-      <Footer />
     </div>
   );
 }

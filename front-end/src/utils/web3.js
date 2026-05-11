8cca963f0b39f5ce1fbabf4345cf37f23164a8cb
avytran <tr.anhvy.work@gmail.com>
2026-05-12 02:27:09 +0700

    feat: env setup

--- front-end/src/utils/web3.js
+++ front-end/src/utils/web3.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import VotingABI from "../abis/Voting.json";
 
-const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const CONTRACT_ADDRESS = import.meta.env.VITE_VOTING_CONTRACT_ADDRESS;
 
 export const getContract = async () => {
   if (!window.ethereum) throw new Error("Please install MetaMask!");
@@ -9,6 +9,10 @@ export const getContract = async () => {
   const provider = new ethers.BrowserProvider(window.ethereum);
   
   const signer = await provider.getSigner();
+
+  if (!CONTRACT_ADDRESS) {
+    console.error("Contract address is missing in .env file");
+  }
   
   return new ethers.Contract(CONTRACT_ADDRESS, VotingABI.abi, signer);
 };
